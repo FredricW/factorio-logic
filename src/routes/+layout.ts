59203b948b4b5395889195bf84099917a4567d48
Fredric Waadeland <fredric.waadeland@gmail.com>
2023-02-05 16:16:40 +0100

    chore: redirect after login

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -8,6 +8,10 @@ export const load: Load = async (event) => {
 
 	if (session?.user?.id) {
 		profile = await supabaseClient.from('profiles').select('*').eq('id', session.user.id).single();
+
+		if (event.url.pathname === '/login') {
+			throw redirect(303, '/');
+		}
 	} else {
 		if (event.url.pathname !== '/login') {
 			throw redirect(303, '/login');
