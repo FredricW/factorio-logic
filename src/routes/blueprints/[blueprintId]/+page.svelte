995d33f225bd4a1c6bd5795c35f49807b2e603d8
Fredric Waadeland <fredric.waadeland@gmail.com>
2023-02-23 15:53:19 +0100

    feat(history): sticky table head

--- src/routes/blueprints/[blueprintId]/+page.svelte
+++ src/routes/blueprints/[blueprintId]/+page.svelte
@@ -150,18 +150,18 @@
 		class="fixed right-0 top-0 h-full flex items-center pointer-events-none"
 	>
 		<div class="pr-4 prose pointer-events-auto">
-			<div class="overflow-scroll max-h-[80vh] shadow-2xl border-2 border-base-100 rounded-lg">
+			<div class="shadow-2xl max-h-[80vh] overflow-y-auto border-2 border-base-100 rounded-lg">
 				{#if !blueprintModule?.history || blueprintModule.history.length === 0}
 					<p>No history</p>
 				{:else}
-					<table class="table my-0 table-compact w-full">
-						<thead>
+					<table class="table my-0 table-compact w-full h-full">
+						<thead class="sticky top-0">
 							<tr>
 								<th>Timestamp</th>
 								<th>Id</th>
 							</tr>
 						</thead>
-						<tbody>
+						<tbody class="overflow-y-auto">
 							{#each blueprintModule.history.reverse() as history}
 								<tr class="hover cursor-pointer">
 									<td>{toCompactDate(history.timestamp)}</td>
