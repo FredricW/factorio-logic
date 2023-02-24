08f86f1d696299471f3a66a496af64cd731936b3
Fredric Waadeland <fredric.waadeland@gmail.com>
2023-02-24 14:30:32 +0100

    feat: minimalistic history design

--- src/lib/components/History.svelte
+++ src/lib/components/History.svelte
@@ -8,39 +8,46 @@
 		const date = new Date(datestring);
 		return `${date.toLocaleDateString()} ${date.toLocaleTimeString()}`;
 	};
+	let currentHistoryId: string | null = null;
+	let activeTooltip: string | null = null;
 </script>
 
 <div
 	transition:fly={{ x: 30, duration: 150 }}
 	class="fixed right-0 top-0 h-full flex items-center pointer-events-none"
 >
-	<div class="pr-4 pointer-events-auto">
-		<div
-			class="shadow-2xl bg-base-200 max-h-[80vh] overflow-y-auto border-2 border-base-100 rounded-box"
-		>
+	<div class="pr-4 pointer-events-auto group">
+		<div class="shadow-2xl bg-base-200 max-h-[80vh] overflow-y-auto border-base-100 rounded-box">
 			{#if !blueprintModule?.history || blueprintModule.history.length === 0}
 				<div class="prose">
 					<p class="px-4">No history</p>
 				</div>
 			{:else}
-				<table class="table my-0 table-compact w-full h-full">
-					<thead class="sticky top-0">
-						<tr>
-							<th>Timestamp</th>
-							<th>Id</th>
-						</tr>
-					</thead>
-					<tbody class="overflow-y-auto">
-						{#each blueprintModule.history.reverse() as history}
-							<tr class="hover cursor-pointer">
-								<td>{toCompactDate(history.timestamp)}</td>
-								<td title={history.id}
-									><span class="text-ellipsis block w-16 overflow-hidden">{history.id}</span></td
-								>
-							</tr>
-						{/each}
-					</tbody>
-				</table>
+				<div
+					on:mouseleave={() => {
+						activeTooltip = null;
+					}}
+					class="btn-group btn-group-vertical"
+				>
+					{#each blueprintModule.history as history, i (history.id)}
+						<button
+							class="btn w-10"
+							class:btn-accent={currentHistoryId ? currentHistoryId === history.id : i === 0}
+							on:click={() => {
+								currentHistoryId = history.id;
+							}}
+							on:mouseenter={() => {
+								activeTooltip = history.id;
+							}}
+							>{i}
+							<span
+								class="tooltip tooltip-left absolute left-0 transition-all"
+								class:tooltip-open={activeTooltip === history.id}
+								data-tip={toCompactDate(history.timestamp)}><div /></span
+							>
+						</button>
+					{/each}
+				</div>
 			{/if}
 		</div>
 	</div>
